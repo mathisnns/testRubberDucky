fa6c7396d1cc9cc80e69b0e156528a6184c71cd8
Mathis Nunes <mathis.nunes.30@gmail.com>
2022-07-09 15:03:26 +0200

    Add files via upload

---
+++ payload.ps1
@@ -0,0 +1,21 @@
+ 
+$url = "https://web-tech.fr/wp-content/uploads/2018/05/TrollFace.jpg"
+$output = "C:\Users\$env:USERNAME\Desktop\bigup.jpg"
+(New-Object System.Net.WebClient).DownloadFile($url, $output)
+$setwallpapersrc = @"
+using System.Runtime.InteropServices;
+public class wallpaper
+{
+public const int SetDesktopWallpaper = 20;
+public const int UpdateIniFile = 0x01;
+public const int SendWinIniChange = 0x02;
+[DllImport("user32.dll", SetLastError = true, CharSet = CharSet.Auto)]
+private static extern int SystemParametersInfo (int uAction, int uParam, string lpvParam, int fuWinIni);
+public static void SetWallpaper ( string path )
+{
+SystemParametersInfo( SetDesktopWallpaper, 0, path, UpdateIniFile | SendWinIniChange );
+}
+}
+"@
+Add-Type -TypeDefinition $setwallpapersrc
+[wallpaper]::SetWallpaper($output)
